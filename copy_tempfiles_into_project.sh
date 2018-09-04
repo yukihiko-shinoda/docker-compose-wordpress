1f67ce29b64731dff4b142349a258b87b0473952
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2018-09-05 03:44:47 +0900

    Remove dependency on host environment

--- copy_tempfiles_into_project.sh
+++ copy_tempfiles_into_project.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 cp -fpR /tmp/wordpress/vendor/* wordpress-s3/vendor/
-cp -fpR /tmp/wordpress/static/* wordpress-s3/web/wp/static/
+cp -fpR /tmp/wordpress/static/* wordpress-s3/static/
---
+++ restore_tempfiles_from_project.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -eu
+cp -fpR wordpress-s3/vendor/* /tmp/wordpress/vendor/
+cp -fpR wordpress-s3/static/* /tmp/wordpress/static/
